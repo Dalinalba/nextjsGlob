132b5d6bbd1ad59fc686776718c78765190f0a99
Dalinalba <148215916+Dalinalba@users.noreply.github.com>
2023-12-09 18:54:33 +0100

    copmplete image

--- globomatics/app/home/page.js
+++ globomatics/app/home/page.js
@@ -1,6 +1,6 @@
 import Link from 'next/link'
 import Image from 'next/image'
-import OurStoryPic from "./../images/home-image-1.jpg"
+import OurStoryPic from "../../images/home-image-1.jpg"
 import styles from "./home.module.css"
 
 export default function Page() {
@@ -10,7 +10,13 @@ export default function Page() {
         <Image
         src= {OurStoryPic}
         alt ="Our Story Pic"
-        quality={100}/>
+        quality={100}
+        placeholder="blur"
+        sizes='100vw'
+        fill
+        style={{
+            objectFit:"cover",
+        }}/>
       </div>
       <h1>
           Welcome to Home page
